34e9c58e492c593b68477fdb44a46d5485fedba0
Sergey <srgsvl@list.ru>
2020-06-15 23:10:20 +0300

    Adding some changes

---
+++ src/components/VisualizeObj.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="card-wrapper">
+    <el-card class="box-card"
+      v-for="(val, name) in obj" :key="name"
+    >
+      <div class="title">
+        <span>{{ name }}</span>
+        <span>{{ val }}</span>
+      </div>
+      <!-- <div class="text">
+        <span>{{ ob.val}}</span>
+      </div> -->
+    </el-card>
+  </div>
+</template>
+
+<script>
+  import '@babel/polyfill'
+  import Vuex from 'vuex'
+  // Настройки темы element-ui
+  import { Card } from 'element-ui'
+  import lang from 'element-ui/lib/locale/lang/ru-RU'
+  import locale from 'element-ui/lib/locale'
+  locale.use(lang)
+  // import {store} from '../store/'
+  // import {mapGetters} from 'vuex'
+  // import {mapMutations} from 'vuex'
+
+  export default {
+    components: {
+      'el-card': Card
+    },
+    props: {
+      obj: {
+        type: Object,
+        default: ''
+      },
+    },
+    data(){
+      return {
+        activeUser: '',
+        activeUserEmail: '',
+        tableName: 'announcement',
+      }
+    },
+    computed: {
+      // ...mapGetters([
+      //   'task',
+      //   'config',
+      // ])
+    },
+    methods: {
+      // ...mapMutations([
+
+      // ])
+    },
+    beforeMount(){
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,61 +1,50 @@
 <template>
   <div class="row-btn">
-    Здесь объект
-    <!-- <portal-btn v-for="(item, index) in buttons"
-      :key="index"
-      :item="item"
-    ></portal-btn> -->
+    <VisualizeObj :obj="Obj"></VisualizeObj>
   </div>
 </template>
 
 <script>
-  import {store} from '../store/'
-  // import {mapMutations} from 'vuex'
+import { store } from '../store/'
+import VisualizeObj from '../components/VisualizeObj'
 
-  export default {
-    data () {
-      return {
-        // buttons: GetButtons,
-        // taskTitle: 'Визуализация объектов JavaScript и их свойств',
-      }
-    },
-    created(){
-      // this.btnHomeOff()
-      // this.btnLogoutOff()
-      // this.btnLoginOff()
-    },
-    components: {
-      // PortalBtn,
-      // GetButtons
-    },
-    methods: {
-      // ...mapMutations([
-      //   'btnHomeOff',
-      //   'btnLogoutOff',
-      //   'btnLoginOff'
-      // ])
-    },
-
-    mounted(){
-      document.title = this.taskTitle
+export default {
+  data() {
+    return {
+      Obj: { el: window.event },
     }
-  }
+  },
+  created() {
+    console.log('obj = ', this.Object)
+  },
+  components: {
+    VisualizeObj,
+  },
+  methods: {
+  },
+
+  mounted() {
+    document.title = this.taskTitle
+  },
+}
 </script>
 
 <style>
-  hr {
-    border-top: 1px solid #ccc !important;
-  }
-  @media screen and (max-width: 1200px) {
-    .input_rgba, .wrap_preview_site, .save_btn {
-      width: 100% !important;
-    }
-    .wrap_preview_site {
-      margin-top: 20px;
-      margin-bottom: 20px;
-    }
-  }
-  textarea {
+hr {
+  border-top: 1px solid #ccc !important;
+}
+@media screen and (max-width: 1200px) {
+  .input_rgba,
+  .wrap_preview_site,
+  .save_btn {
     width: 100% !important;
   }
+  .wrap_preview_site {
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+}
+textarea {
+  width: 100% !important;
+}
 </style>
